ee0ac1c57b2239e25c018a82709bdba0daaae2ae
Matheus Bruno <matheusferreirabruno@hotmail.com>
2024-02-15 14:35:17 -0300

    Update webpack configuration and package.json scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,143 +2,149 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production'
-  let entry, minimize
-
-  if (env && env.minimize) {
-    entry = {
-      'quill-better-table-plus.min.js': ['./src/quill-better-table-plus.js'],
-    }
-    minimize = true
-  } else {
-    entry = {
-      'quill-better-table-plus.js': ['./src/quill-better-table-plus.js'],
-      'quill-better-table-plus': './src/assets/quill-better-table-plus.less',
-      'demo/demo.js': './demo/js/demo.js',
-    }
-    minimize = false
-  }
-
-  return {
-    entry,
-
-    optimization: {
-      minimize,
-    },
-
-    output: {
-      filename: '[name]',
-      library: 'QuillBetterTablePlus',
-      libraryExport: 'default',
-      libraryTarget: 'umd',
-      path: path.resolve(__dirname, './dist/'),
-    },
-
-    resolve: {
-      alias: {
-        'src': path.resolve(__dirname, './src'),
-        'dist': path.resolve(__dirname, './dist'),
-      },
-      extensions: ['.js', '.less', '.html'],
-    },
-
-    externals: {
-      'quill': {
-        commonjs: 'quill',
-        commonjs2: 'quill',
-        amd: 'quill',
-        root: 'Quill',
-      },
-    },
-
-    module: {
-      rules: [
-        {
-          test: /\.(jpg|jpeg|png)$/,
-          include: [
-            path.resolve(__dirname, '../src/assets/imgs'),
-          ],
-          use: [{
-            loader: 'url-loader',
-            options: {
-              limit: 8192,
-            },
-          }],
-        },
-
-        {
-          test: /\.(html|svg)$/,
-          use: [{
-            loader: 'html-loader',
-            options: {
-              minimize: true,
-            },
-          }],
-        },
-
-        {
-          test: /\.less$/,
-          use: [
-            // fallback to style-loader in development
-            !isProduction ? 'style-loader' : MiniCssExtractPlugin.loader,
-            'css-loader',
-            'less-loader',
-          ],
-        },
-
-        {
-          test: /\.js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/env',
-                  {
-                    targets: {
-                      browsers: [
-                        'last 2 Chrome major versions',
-                        'last 2 Firefox major versions',
-                        'last 2 Safari major versions',
-                        'last 2 Edge major versions',
-                        'last 2 iOS major versions',
-                        'last 2 ChromeAndroid major versions',
-                      ],
-                    },
-                  },
-                ],
+const jsRules = {
+  test: /\.js$/,
+  exclude: /(node_modules|bower_components)/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: [
+        [
+          '@babel/env',
+          {
+            targets: {
+              browsers: [
+                'last 2 Chrome major versions',
+                'last 2 Firefox major versions',
+                'last 2 Safari major versions',
+                'last 2 Edge major versions',
+                'last 2 iOS major versions',
+                'last 2 ChromeAndroid major versions',
               ],
             },
           },
-        },
+        ],
       ],
     },
+  },
+};
 
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: 'quill-better-table',
-        template: './demo/demo.html',
-        filename: 'demo/demo.html',
-      }),
-
-      new MiniCssExtractPlugin({
-        filename: '[name].css',
-        chunkFilename: '[name].[id].css',
-      }),
-
-      new webpack.HotModuleReplacementPlugin({}),
-    ],
-
-    devServer: {
-      host: 'localhost',
-      contentBase: path.join(__dirname, './dist'),
-      port: 8080,
-      hot: false,
-      open: true,
-      openPage: "demo/demo.html",
+const htmlSvgRules = {
+  test: /\.(html|svg)$/,
+  use: [{
+    loader: 'html-loader',
+    options: {
+      minimize: true,
     },
+  }],
+};
+
+const lessRules = {
+  test: /\.less$/,
+  use: [
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+    'less-loader',
+  ],
+};
+
+const imgRules = {
+  test: /\.(jpg|jpeg|png)$/,
+  include: [
+    path.resolve(__dirname, '../src/assets/imgs'),
+  ],
+  use: [{
+    loader: 'url-loader',
+    options: {
+      limit: 8192,
+    },
+  }],
+};
+
+const baseConfig = {
+  mode: 'development',
+  entry: {
+    'quill-better-table-plus.js': ['./src/quill-better-table-plus.js'],
+    'quill-better-table-plus': './src/assets/quill-better-table-plus.less',
+    'demo/demo.js': './demo/js/demo.js',
+  },
+  output: {
+    filename: '[name]',
+    library: 'QuillBetterTablePlus',
+    libraryExport: 'default',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, './dist/'),
+    clean: true,
+  },
+  resolve: {
+    alias: {
+      'src': path.resolve(__dirname, './src'),
+      'dist': path.resolve(__dirname, './dist'),
+    },
+    extensions: ['.js', '.less', '.html'],  
+  },
+  externals: {
+    'quill': {
+      commonjs: 'quill',
+      commonjs2: 'quill',
+      amd: 'quill',
+      root: 'Quill',
+    },
+  },
+  devtool: 'eval-cheap-source-map',
+  module: {
+    rules: [jsRules, htmlSvgRules, lessRules, imgRules],
+    noParse: [
+      /\/node_modules\/clone\/clone\.js$/,
+      /\/node_modules\/eventemitter3\/index\.js$/,
+      /\/node_modules\/extend\/index\.js$/,
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'quill-better-table',
+      template: './demo/demo.html',
+      filename: 'demo/demo.html',
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[name].[id].css',
+    }),
+
+    new webpack.HotModuleReplacementPlugin({}),
+  ],
+  devServer: {
+    host: 'localhost',
+    static: {
+      directory: path.join(__dirname, "./dist")
+    },
+    port: 8080,
+    hot: false,
+    open: true
+  },
+};
+
+module.exports = env => {
+  if (env?.minimize) {    
+    const { devServer, ...prodConfig } = baseConfig;
+    return {
+      ...prodConfig,
+      mode: 'production',
+      entry: {
+        'quill-better-table-plus.min.js': ['./src/quill-better-table-plus.js'],
+        'quill-better-table-plus': './src/assets/quill-better-table-plus.less',
+    },
+      devtool: false,      
+      optimization: {
+        minimizer: [
+          new CssMinimizerPlugin(),
+        ],
+      },
+    };
   }
-}
+  
+  return baseConfig;
+};
